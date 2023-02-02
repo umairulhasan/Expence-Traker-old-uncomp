7d9e309b063c935b5cdd2184e65eda4614e4ca82
Umair <mrumairhassan@outlook.com>
2023-02-02 22:41:16 +0500

    Updated

---
+++ src/proVsNoop.js
@@ -0,0 +1,51 @@
+// Short cut of conditional
+
+// if(doc);{
+//     goToDoc()
+// }
+
+//     pro
+// doc && goToDoc()
+
+
+// Array to object
+ 
+// let arr = ["value1","value2","value3"];
+// let arrObj = {}
+// for (i=0; i<arr.length; i++){
+//     if (arr[1]==! undefined){
+//         arrObj[i] == arr[i];
+//     }
+// }
+
+
+// let arr = ["value1", "value2", "value3"];
+// let arrObj ={...arr};
+
+
+
+
+// populating of array 
+
+// for(let i= 0; i<arraySize; i++){
+
+//     filledArray[i] {'hello' : 'goodbye'};
+// }
+
+
+// let filledArray = new Array(arraySize).fill(null).map(()=>({'hello' : 'goodbye'}));
+
+// the ternery operator is
+
+// let hungery = true ;
+// let eat ;
+// if(hungery ==true){
+//     eat = 'yes';
+
+// } else {
+//     eat = 'no';
+// }
+//  pro
+
+// let hungry = true;
+// let eat = hungery == true ? 'yes' : no;
