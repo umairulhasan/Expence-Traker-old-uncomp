4f300c07902f84bcd9d17768d49b012ec3e071d9
Umair <mrumairhassan@outlook.com>
2023-02-27 02:11:18 +0500

    Add comment whats props do in background use object

--- src/propsPractice.js
+++ src/propsPractice.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function propsPractice(props) {
   return (
+    // in back ground prop is use as a object
+
+    // props {
+
+    //  dishName :'This is the parent data'
+
+    // }
     <div>
-      <h1>
-
-Hello {props.displayName}
-
-      </h1>
+      <h1>Hello {props.displayName}</h1>
     </div>
   );
 }
